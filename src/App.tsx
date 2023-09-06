af66667a4dcd45e32a290b997225f61e3ea99701
psamirt <p_samir@hotmail.com>
2023-09-06 09:05:13 -0500

    spiner loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Navbar from './components/Navbar'
+import { useState, useEffect } from 'react'
+import { Bars } from 'react-loader-spinner'
 
 const App = (): JSX.Element => {
+	const [loading, setLoading] = useState(true)
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false)
+		}, 5000)
+	}, [])
+
 	return (
-		<div className='relative'>
-			<video
-				autoPlay
-				loop
-				muted
-				className='fixed w-full h-full object-cover z-[-1]'
-			>
-				<source src='background.mp4' type='video/mp4' />
-			</video>
-			<Navbar />
-			<Routes>
-				<Route path='/' element={<Home />} />
-			</Routes>
+		<div>
+			{loading ? (
+				<div className='flex justify-center items-center h-screen'>
+					<Bars
+						height='80'
+						width='80'
+						ariaLabel='bars-loading'
+						visible={true}
+						color='#ffffff'
+					/>
+				</div>
+			) : (
+				<div className='relative'>
+					<video
+						autoPlay
+						loop
+						muted
+						className='fixed w-full h-full object-cover z-[-1]'
+					>
+						<source src='background.mp4' type='video/mp4' />
+					</video>
+					<Navbar />
+					<Routes>
+						<Route path='/' element={<Home />} />
+					</Routes>
+				</div>
+			)}
 		</div>
 	)
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,11 +5,11 @@ const Home = (): JSX.Element => {
 		<div className='flex w-full h-full py-16 px-4 items-center justify-around '>
 			{/* Contenedor del texto */}
 			<div className='max-w-2xl text-center'>
-				<p  className='text-sm md:text-base lg:text-xl font-special'>
-					Soy un desarrollador Front-end con sólida base en tecnologías clave 
-					como JavaScript, Node.js, HTML, CSS, Bootstrap, Tailwind y Next.js. 
-					Comprometido con la excelencia, habituado a trabajar con tecnologías 
-					ágiles y autodidacta. He desarrollado proyectos personales y grupales 
+				<p className='text-sm md:text-base lg:text-xl font-special'>
+					Soy un desarrollador Front-end con sólida base en tecnologías clave
+					como JavaScript, Node.js, HTML, CSS, Bootstrap, Tailwind y Next.js.
+					Comprometido con la excelencia, habituado a trabajar con tecnologías
+					ágiles y autodidacta. He desarrollado proyectos personales y grupales
 					demostrando creatividad y capacidad de resolución de problemas.
 				</p>
 			</div>
