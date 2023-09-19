51c7ed1e9fe7a4db0b2e5b0553e36e9a804210b0
psamirt <p_samir@hotmail.com>
2023-09-19 17:39:50 -0500

    nuevo proyecto agregado

--- client/src/pages/projects/Projects.tsx
+++ client/src/pages/projects/Projects.tsx
@@ -8,6 +8,8 @@ import {
 	BiLogoPostgresql,
 	BiLogoMongodb,
 	BiLogoGithub,
+	BiLogoAngular,
+	BiLogoHtml5,
 } from 'react-icons/bi'
 import { SiNextdotjs } from 'react-icons/si'
 
@@ -79,7 +81,7 @@ const Projects = (): JSX.Element => {
 
 			{/* gamerzone */}
 			<div className='items-center flex flex-col'>
-			<div className='hover:scale-[1.02] ease-in duration-300 '>
+				<div className='hover:scale-[1.02] ease-in duration-300 '>
 					<figure className='rounded-lg shadow-lg p-6 w-[300px] h-[510px] bg-black bg-opacity-75 m-6 ring-4 ring-white'>
 						<div className='mb-4'>
 							<img
@@ -146,7 +148,7 @@ const Projects = (): JSX.Element => {
 
 			{/* ReStore */}
 			<div className='items-center flex flex-col'>
-			<div className='hover:scale-[1.02] ease-in duration-300 '>
+				<div className='hover:scale-[1.02] ease-in duration-300 '>
 					<figure className='rounded-lg shadow-lg p-6 w-[300px] h-[510px] bg-black bg-opacity-75 m-6 ring-4 ring-white'>
 						<div className='mb-4'>
 							<img
@@ -195,6 +197,70 @@ const Projects = (): JSX.Element => {
 								<li className='flex flex-col items-center'>
 									<BiLogoMongodb size={35} className='text-green-700' />
 									<span className='text-xs'>MongoDB</span>
+								</li>
+							</ul>
+						</div>
+					</figure>
+				</div>
+				<div className='items-center flex justify-evenly w-full'>
+					<a
+						href='https://re-store-six.vercel.app/'
+						target='_blank'
+						className=' bg-white rounded-full text-black font-bold p-2 hover:translate-y-[0.7px] hover:bg-slate-200'
+					>
+						<span>ReStore</span>
+					</a>{' '}
+					<a
+						href='https://github.com/psamirt/ReStore'
+						target='_blank'
+						className='hover:translate-y-[0.7px] hover:text-gray-200'
+					>
+						<BiLogoGithub size={45} />
+					</a>
+				</div>
+			</div>
+			{/* ReStore */}
+			<div className='items-center flex flex-col'>
+				<div className='hover:scale-[1.02] ease-in duration-300 '>
+					<figure className='rounded-lg shadow-lg p-6 w-[300px] h-[510px] bg-black bg-opacity-75 m-6 ring-4 ring-white'>
+						<div className='mb-4'>
+							<img
+								src='imagenes/sesion.png'
+								alt='restore'
+								className='w-96 h-auto mx-auto'
+							/>
+						</div>
+						<div className='mb-4'>
+							<h1 className='text-xl font-bold font-roboto text-center'>
+								Login Page
+							</h1>
+							<p className=' text-center italic'>
+								Pagina para poder iniciar sesión usando Api rest, guards y
+								lazyload
+							</p>
+						</div>
+						<div>
+							<h3 className='text-xl font-semibold mb-2 font-roboto text-center'>
+								Tecnologías Aplicadas
+							</h3>
+							<ul className='flex items-center justify-center space-x-4 flex-wrap'>
+								<li className='flex flex-col items-center'>
+									<BiLogoAngular
+										size={35}
+										className='text-red-500 bg-white rounded-full'
+									/>
+									<span className='text-xs'>Angular</span>
+								</li>
+								<li className='flex flex-col items-center'>
+									<BiSolidFileCss size={35} className='text-gray-600' />
+									<span className='text-xs'>CSS</span>
+								</li>
+								<li className='flex flex-col items-center'>
+									<BiLogoHtml5
+										size={35}
+										className='text-orange-600 bg-white rounded-full'
+									/>
+									<span className='text-xs'>CSS</span>
 								</li>
 							</ul>
 						</div>
